84de290514cc802f7cf422b1a28ff88d10d1622a
Mattias Strömdahl <Mattias.Strmdahl@student.kyh.com>
2020-09-10 14:55:52 +0200

    Added besimmus

--- src/com/company/PrintMean.java
+++ src/com/company/PrintMean.java
@@ -16,7 +16,7 @@ public class PrintMean {
         System.out.println("Mean of 1, 2, 3, 4: " + meanCalc(1d, 2d, 3d, 4d));
         System.out.println("Mean of 1, -2, 3, -4: " + meanCalc(1d, -2d, 3d, -4d));
         System.out.println("Mean of -1, -11, 24, 2: " + meanCalc(-1d, -11d, 24d, 2d));
-
+        // besim bus gllar mus
     }
 
     public static String meanCalc(double a, double b, double c, double d){
