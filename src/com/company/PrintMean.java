06f559553241ee54c0c174c3fc7073cc1e4bb965
Mattias Strömdahl <Mattias.Strmdahl@student.kyh.com>
2020-09-10 14:51:46 +0200

    commit of branch mattias

--- src/com/company/PrintMean.java
+++ src/com/company/PrintMean.java
@@ -3,13 +3,6 @@ package com.company;
 public class PrintMean {
 
     public static void main(String[] args) {
-        //New test comment
-        // Branch Master
-        // Branch Master
-        // Branch Master
-        // Branch Master
-        // Branch Master
-        // Branch Master
 
         //Print mean
         System.out.println("Mean of 1, 1, 3, 3: " + meanCalc(1d, 1d, 3d, 3d));
@@ -17,6 +10,10 @@ public class PrintMean {
         System.out.println("Mean of 1, -2, 3, -4: " + meanCalc(1d, -2d, 3d, -4d));
         System.out.println("Mean of -1, -11, 24, 2: " + meanCalc(-1d, -11d, 24d, 2d));
 
+
+        //Detta är en test av branch mattias
+        //HIHI
+        //
     }
 
     public static String meanCalc(double a, double b, double c, double d){
