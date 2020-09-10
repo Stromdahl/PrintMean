b8fab250779017d3bd97ea2d275ff7dfd665d972
Mattias Strömdahl <Mattias.Strmdahl@student.kyh.com>
2020-09-10 11:39:57 +0200

    Added project files

---
+++ src/com/company/PrintMean.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class PrintMean {
+
+    public static void main(String[] args) {
+        System.out.println("Mean of 1, 1, 3, 3: " + meanCalc(1d, 1d, 3d, 3d));
+        System.out.println("Mean of 1, 2, 3, 4: " + meanCalc(1d, 2d, 3d, 4d));
+        System.out.println("Mean of 1, -2, 3, -4: " + meanCalc(1d, -2d, 3d, -4d));
+        System.out.println("Mean of -1, -11, 24, 2: " + meanCalc(-1d, -11d, 24d, 2d));
+
+    }
+
+    public static String meanCalc(double a, double b, double c, double d){
+        double mean = (a + b + c + d) / 4;
+        String meanDouble = Double.toString(mean);
+        return meanDouble;
+    }
+}
